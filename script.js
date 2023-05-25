6ed036e3e1c38474532b532cc43385e9ea729695
tleanne1 <114450881+tleanne1@users.noreply.github.com>
2023-05-25 16:20:12 -0500

    Added matchCard function

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
 const cards = document.querySelectorAll(".card");
 
 let cardOne, cardTwo;
+let disableDeck = false;
 
 function flipCard(e) {
   let clickedCard = e.target; // getting user clicked card
-  if (clickedCard !== cardOne) {
+  if (clickedCard !== cardOne && !disableDeck) {
     clickedCard.classList.add("flip");
     if (!cardOne) {
       // return the cardOne value to clickCard
       return (cardOne = clickedCard);
     }
     cardTwo = clickedCard;
-
-    let cardOneImg = cardOne.querySelector("img"),
-      cardTwoImg = cardTwo.querySelector("img");
+    disableDeck = true;
+    let cardOneImg = cardOne.querySelector("img").src,
+      cardTwoImg = cardTwo.querySelector("img").src;
     matchCards(cardOneImg, cardTwoImg);
   }
+}
+
+function matchCards(img1, img2) {
+  if (img1 === img2) {
+    // if two cards img matched
+    cardOne.removeEventListener("click", flipCard);
+    cardTwo.removeEventListener("click", flipCard);
+    cardOne = cardTwo = ""; // setting both card values to blank
+    return (disableDeck = false);
+  }
+  // if two card not matched
+  setTimeout(() => {
+    // adding shake class to both card after 400ms
+    cardOne.classList.add("shake");
+    cardTwo.classList.add("shake");
+  }, 400);
+
+  setTimeout(() => {
+    // removing both shake & flip classes from the both cards after 1.2 seconds
+    cardOne.classList.remove("shake", "flip");
+    cardTwo.classList.remove("shake", "flip");
+    cardOne = cardTwo = ""; // setting both card values to blank
+    disableDeck = false;
+  }, 1200);
 }
 
 cards.forEach((card) => {
