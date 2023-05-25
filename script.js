9a4056d03b5147dd8dae65ceabc73ffcb03d4564
tleanne1 <114450881+tleanne1@users.noreply.github.com>
2023-05-25 18:42:58 -0500

    Added shuffleCard function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const cards = document.querySelectorAll(".card");
 
+let matchedCard = 0;
 let cardOne, cardTwo;
 let disableDeck = false;
 
@@ -22,6 +23,13 @@ function flipCard(e) {
 function matchCards(img1, img2) {
   if (img1 === img2) {
     // if two cards img matched
+    matchedCard++; // increment matched value by 1
+    // if matched calue is 8 that means user has matched all the cards (8 * 2 = 16 cards)
+    if (matchedCard == 8) {
+      setTimeout(() => {
+        return shuffleCard();
+      }, 1000); // calling shufflecard function after 1 sec
+    }
     cardOne.removeEventListener("click", flipCard);
     cardTwo.removeEventListener("click", flipCard);
     cardOne = cardTwo = ""; // setting both card values to blank
@@ -43,7 +51,26 @@ function matchCards(img1, img2) {
   }, 1200);
 }
 
+function shuffleCard() {
+  matchedCard = 0;
+  cardOne = cardTwo = "";
+  // creating array of 16 items and each item is repeated twice
+  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
+  arr.sort(() => (Math.random() > 0.5 ? 1 : -1)); // sorting array item randomly
+
+  // removing flip class from all cards and passing random image to each card
+  cards.forEach((card, index) => {
+    card.classList.remove("flip");
+    let imgTag = card.querySelector("img");
+    imgTag.src = `images/img-${arr[index]}.png`;
+    card.addEventListener("click", flipCard);
+  });
+}
+
+shuffleCard();
+
 cards.forEach((card) => {
   // adding click event to all cards
+  card.classList.add("flip");
   card.addEventListener("click", flipCard);
 });
